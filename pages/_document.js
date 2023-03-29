00b23b0c60e14f9566193f1aae14ab07fc453a2a
Lawal Ridwan <lekanlawal83@gmail.com>
2023-03-29 15:49:47 -0400

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Midjourney V5 Prompt Generation" key="title"/>
+        <meta property="og:description" content="Use Midjourney V5 Prompts Generator to unleash your writing potential! Our AI-powered tool provides endless prompts to inspire your writing journey.
+" key="description"/>
         <meta
           property="og:image"
           content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
