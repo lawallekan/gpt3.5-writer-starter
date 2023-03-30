ece682394ed527c40a81f490ce64aee49b662a5a
Lawal Ridwan <lekanlawal83@gmail.com>
2023-03-30 00:50:26 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -40,19 +40,6 @@ const Home = () => {
   return (
     <div className="root">
       <Head>
-    <div class="company-logo">D</div>
-    <nav class="navbar">
-      <ul class="nav-items">
-        <li class="nav-item"><a href="#" class="nav-link">HOME</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">OFFER</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">SHOP</a></li>
-        <li class="nav-item"><a href="#" class="nav-link">CONTACT</a></li>
-      </ul>
-    </nav>
-    <div class="menu-toggle">
-      <i class="bx bx-menu"></i>
-      <i class="bx bx-x"></i>
-    </div>
         <title>Midjourney V5 Prompt Generation</title>
       </Head>
       <div className="container">
